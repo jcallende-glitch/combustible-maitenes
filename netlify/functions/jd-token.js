f82cdb8aaaba155cb9366987a58d2521216cbb73
jcallende-glitch <jcallende@gmail.com>
2026-06-23 21:08:16 -0400

    Add Basic Auth header for token request

--- netlify/functions/jd-token.js
+++ netlify/functions/jd-token.js
@@ -17,14 +17,19 @@ exports.handler = async function(event) {
     const JD_CLIENT_SECRET = process.env.JD_CLIENT_SECRET;
     const JD_TOKEN_URL = 'https://signin.johndeere.com/oauth2/aus78tnlaysMraFhC1t7/v1/token';
 
-    const params = new URLSearchParams({ grant_type, client_id: JD_CLIENT_ID, client_secret: JD_CLIENT_SECRET });
+    const credentials = Buffer.from(`${JD_CLIENT_ID}:${JD_CLIENT_SECRET}`).toString('base64');
+
+    const params = new URLSearchParams({ grant_type });
     if (redirect_uri) params.append('redirect_uri', redirect_uri);
     if (grant_type === 'authorization_code') params.append('code', code);
     if (grant_type === 'refresh_token') params.append('refresh_token', refresh_token);
 
     const resp = await fetch(JD_TOKEN_URL, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${credentials}`
+      },
       body: params
     });
 
