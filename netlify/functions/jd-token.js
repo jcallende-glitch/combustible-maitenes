0f9a257982f89d59bc43b739deb3fc69e28ca862
jcallende-glitch <jcallende@gmail.com>
2026-06-23 20:53:36 -0400

    Refactor JD token function to use redirect_uri

--- netlify/functions/jd-token.js
+++ netlify/functions/jd-token.js
@@ -11,25 +11,20 @@ exports.handler = async function(event) {
 
   try {
     const body = JSON.parse(event.body || '{}');
-    const { grant_type, code, code_verifier, refresh_token } = body;
+    const { grant_type, code, redirect_uri, refresh_token } = body;
 
     const JD_CLIENT_ID = '0oavbnyqn09EHHLnh5d7';
     const JD_CLIENT_SECRET = process.env.JD_CLIENT_SECRET;
-    const JD_REDIRECT_URI = 'https://combustiblelosmaitenes.netlify.app/callback';
     const JD_TOKEN_URL = 'https://signin.johndeere.com/oauth2/aus78tnlaysMraFhC1t7/v1/token';
 
-    const credentials = Buffer.from(`${JD_CLIENT_ID}:${JD_CLIENT_SECRET}`).toString('base64');
-
-    const params = new URLSearchParams({ grant_type, redirect_uri: JD_REDIRECT_URI });
-    if (grant_type === 'authorization_code') { params.append('code', code); params.append('code_verifier', code_verifier); }
-    if (grant_type === 'refresh_token') { params.append('refresh_token', refresh_token); }
+    const params = new URLSearchParams({ grant_type, client_id: JD_CLIENT_ID, client_secret: JD_CLIENT_SECRET });
+    if (redirect_uri) params.append('redirect_uri', redirect_uri);
+    if (grant_type === 'authorization_code') params.append('code', code);
+    if (grant_type === 'refresh_token') params.append('refresh_token', refresh_token);
 
     const resp = await fetch(JD_TOKEN_URL, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Basic ${credentials}`
-      },
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: params
     });
 
